a3ccf72cd805bf1b6838fb61e687276df2c0a15c
William Shackleton <w.shackleton@gmail.com>
2012-05-04 22:03:08 +0100

    Continued adding XML parsing, added joystick file scanning. JS layouts tested, not tested with client.

--- src/uk/digitalsquid/droidpad2/xml/LayoutDecoder.java
+++ src/uk/digitalsquid/droidpad2/xml/LayoutDecoder.java
@@ -16,6 +16,11 @@ import uk.digitalsquid.droidpad2.LogTag;
 import uk.digitalsquid.droidpad2.buttons.Button;
 import uk.digitalsquid.droidpad2.buttons.Layout;
 import uk.digitalsquid.droidpad2.buttons.ModeSpec;
+import uk.digitalsquid.droidpad2.buttons.Slider;
+import uk.digitalsquid.droidpad2.buttons.Slider.SliderType;
+import uk.digitalsquid.droidpad2.buttons.ToggleButton;
+import uk.digitalsquid.droidpad2.buttons.TouchPanel;
+import uk.digitalsquid.droidpad2.buttons.TouchPanel.PanelType;
 import android.sax.Element;
 import android.sax.ElementListener;
 import android.sax.RootElement;
@@ -52,7 +57,7 @@ public class LayoutDecoder implements LogTag {
 	
 	private static final class DocumentListener {
 		Layout layout;
-		int mode;
+		int mode = ModeSpec.LAYOUTS_JS;
 		
 		RootElement root = new RootElement("layout");
 		
@@ -81,6 +86,27 @@ public class LayoutDecoder implements LogTag {
 					int height = Math.abs(getInt(attr, "height", Layout.BUTTONS_Y));
 					
 					layout = new Layout(title, description, width, height);
+					
+					String modeText = attr.getValue("mode");
+					if(modeText == null) modeText = "";
+					
+					if(modeText.equals("js")) mode = ModeSpec.LAYOUTS_JS;
+					if(modeText.equals("joystick")) mode = ModeSpec.LAYOUTS_JS;
+					
+					if(modeText.equals("mouse")) mode = ModeSpec.LAYOUTS_MOUSE;
+					
+					if(modeText.equals("absmouse") || modeText.equals("point") || modeText.equals("pointer")) {
+						mode = ModeSpec.LAYOUTS_MOUSE_ABS;
+						layout.setExtraDetail(Layout.EXTRA_MOUSE_ABSOLUTE);
+					}
+					
+					if(modeText.equals("trackpad") || modeText.equals("laptop")) {
+						mode = ModeSpec.LAYOUTS_MOUSE;
+						layout.setExtraDetail(Layout.EXTRA_MOUSE_TRACKPAD);
+					}
+					
+					if(modeText.equals("slide")) mode = ModeSpec.LAYOUTS_SLIDE;
+					if(modeText.equals("slideshow")) mode = ModeSpec.LAYOUTS_SLIDE;
 				}
 				
 				@Override
@@ -107,7 +133,90 @@ public class LayoutDecoder implements LogTag {
 				@Override
 				public void end(String text) {
 					if(text == null) text = "Button";
-					layout.add(new Button(x, y, width, height, text, textSize));
+					layout.add(new Button(x, y, width, height, text, textSize).setResetButton(isReset));
+				}
+			});
+			
+			Element toggle = root.getChild("toggle");
+			toggle.setTextElementListener(new TextElementListener() {
+				
+				int x, y, width, height, textSize;
+				
+				@Override
+				public void start(Attributes attr) {
+					x = getAbsInt(attr, "x", 0);
+					y = getAbsInt(attr, "y", 0);
+					width = getAbsInt(attr, "width", 1);
+					height = getAbsInt(attr, "height", 1);
+					textSize = getAbsInt(attr, "textSize", 0);
+				}
+				
+				@Override
+				public void end(String text) {
+					if(text == null) text = "Toggle";
+					layout.add(new ToggleButton(x, y, width, height, text, textSize));
+				}
+			});
+			
+			Element slider = root.getChild("slider");
+			slider.setElementListener(new ElementListener() {
+				
+				int x, y, width, height;
+				SliderType type;
+				
+				@Override
+				public void start(Attributes attr) {
+					x = getAbsInt(attr, "x", 0);
+					y = getAbsInt(attr, "y", 0);
+					width = getAbsInt(attr, "width", 1);
+					height = getAbsInt(attr, "height", 1);
+					
+					String typeName = attr.getValue("type");
+					if(typeName == null) typeName = "both";
+					if(typeName.equalsIgnoreCase("x")) type = SliderType.X;
+					if(typeName.equalsIgnoreCase("y")) type = SliderType.Y;
+					if(typeName.equalsIgnoreCase("horizontal")) type = SliderType.X;
+					if(typeName.equalsIgnoreCase("vertical")) type = SliderType.Y;
+					
+					if(typeName.equalsIgnoreCase("xy")) type = SliderType.Both;
+					if(typeName.equalsIgnoreCase("both")) type = SliderType.Both;
+					if(typeName.equalsIgnoreCase("dual")) type = SliderType.Both;
+				}
+				
+				@Override
+				public void end() {
+					layout.add(new Slider(x, y, width, height, type));
+				}
+			});
+			
+			Element panel = root.getChild("panel");
+			panel.setElementListener(new ElementListener() {
+				
+				int x, y, width, height;
+				PanelType type;
+				
+				@Override
+				public void start(Attributes attr) {
+					x = getAbsInt(attr, "x", 0);
+					y = getAbsInt(attr, "y", 0);
+					width = getAbsInt(attr, "width", 1);
+					height = getAbsInt(attr, "height", 1);
+					
+					String typeName = attr.getValue("type");
+					if(typeName == null) typeName = "both";
+					if(typeName.equalsIgnoreCase("x")) type = PanelType.X;
+					if(typeName.equalsIgnoreCase("y")) type = PanelType.Y;
+					if(typeName.equalsIgnoreCase("horizontal")) type = PanelType.X;
+					if(typeName.equalsIgnoreCase("vertical")) type = PanelType.Y;
+					
+					if(typeName.equalsIgnoreCase("xy")) type = PanelType.Both;
+					if(typeName.equalsIgnoreCase("both")) type = PanelType.Both;
+					if(typeName.equalsIgnoreCase("dual")) type = PanelType.Both;
+				}
+				
+				@Override
+				public void end() {
+					layout.add(new TouchPanel(x, y, width, height, type));
 				}
 			});
 		}
@@ -120,18 +229,6 @@ public class LayoutDecoder implements LogTag {
 			
 			try {
 				return Integer.valueOf(value);
-			} catch(NumberFormatException e) {
-				return defaultValue;
-			} catch(NullPointerException e) {
-				return defaultValue;
-			}
-		}
-		
-		private float getFloat(Attributes attr, String name, float defaultValue) {
-			String value = attr.getValue(name);
-			
-			try {
-				return Float.valueOf(value);
 			} catch(NumberFormatException e) {
 				return defaultValue;
 			} catch(NullPointerException e) {
